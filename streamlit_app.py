b8a5c39806fccb373d9dc6e6378b00717801c027
Alan Q Testing <104699224+AlanQTesting@users.noreply.github.com>
2022-04-30 21:41:45 -0700

    Update streamlit_app.py
    add new fruit list to mennu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Mom\'s New Healthy Dinner')
 
@@ -8,3 +9,7 @@ streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🫐')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
