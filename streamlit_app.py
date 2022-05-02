dbed1be01ef20bca6070485f01ed8472c326060c
Alan Q Testing <104699224+AlanQTesting@users.noreply.github.com>
2022-05-01 20:23:15 -0700

    Update streamlit_app.py
    fix default selection issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,10 @@ streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🫐')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"), ['Avocado', ['Strawberries']
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:" , list(my_fruit_list.index), ['Avocado', ['Strawberries'])
 
 # streamlit.dataframe(my_fruit_list)
 
