06826b3d00da72f29c0ec6d2f3c572a4cd470ce7
Alan Q Testing <104699224+AlanQTesting@users.noreply.github.com>
2022-05-01 20:24:00 -0700

    Update streamlit_app.py
    fix default selection code

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,10 @@ streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🫐')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:" , list(my_fruit_list.index), ['Avocado', ['Strawberries'])
-
 # streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:" , list(my_fruit_list.index), ['Avocado', ['Strawberries'])
+                                                                        
 # Display the table on the page
 streamlit.dataframe(my_fruit_list)
