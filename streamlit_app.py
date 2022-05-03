4fec4d0d4f48efd7058bd949ac40447b8e253752
Alan Q Testing <104699224+AlanQTesting@users.noreply.github.com>
2022-05-02 22:46:53 -0700

    Update streamlit_app.py
    Move placement of fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,6 @@ streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🫐')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# new section to display fruityvice
-streamlit.header('Fruityvice Fruit Advice!')
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 # streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list so they can pick the fruit they want to include
@@ -28,3 +22,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
                                                                         
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+# new section to display fruityvice
+streamlit.header('Fruityvice Fruit Advice!')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
