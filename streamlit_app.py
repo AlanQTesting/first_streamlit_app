427ee056b8e3c0f0aac00141ae84dac5fbb9c3bf
Alan Q Testing <104699224+AlanQTesting@users.noreply.github.com>
2022-05-02 22:44:00 -0700

    Update streamlit_app.py
    add fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Mom\'s New Healthy Dinner')
 
@@ -12,6 +13,10 @@ streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🫐')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# new section to display fruityvice
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 # streamlit.dataframe(my_fruit_list)
 
